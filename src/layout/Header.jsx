e91761a42efe02f26b7dcfb46c3c0ee3f2c29c78
Niyati Patil <109764410+niyatipatil@users.noreply.github.com>
2024-04-12 00:08:25 +0530

    Add files via upload

---
+++ src/layout/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="header-container">
+      <img className="logo" src="https://faoschwarz.com/cdn/shop/files/faologonew_x120@2x.png?v=1624376052" href="#" alt="logo" />
+      <label>The Book Store</label>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/layout/Home.jsx
@@ -0,0 +1,7 @@
+const Home = (props) => {
+  console.log(props);
+
+  return <div>This is home</div>;
+};
+
+export default Home;
